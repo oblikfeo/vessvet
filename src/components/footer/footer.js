de772a587b9144a5958cec7b344f10872a1b72bc
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-02-17 04:12:56 +0600

    mail

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,3 +1,4 @@
+'use client'
 import styles from "./footer.module.css";
 import footer from '../../../public/img/footer.png'
 import Image from "next/image";
@@ -8,8 +9,34 @@ import water from "../../../public/img/water.svg"
 import what from "../../../public/img/what.png"
 import tg from "../../../public/img/tg.png"
 import inst from "../../../public/img/inst.png"
+import emailjs from 'emailjs-com';
+import { useState } from "react";
+
+const SERVICE_ID = 'service_dq5xm29';
+const TEMPLATE_ID = 'template_3venlpe';
+const USER_ID = 'v-7auv5vOY-kY5tiN'; // это почему то public key
 
 export default function Footer() {
+
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const templateParams = {
+            name,
+            phone
+        };
+        emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, USER_ID)
+            .then(response => {
+                console.log('SUCCESS!', response.status, response.text);
+                setName('')
+                setPhone('')
+            }, error => {
+                console.log('FAILED...', error);
+            });
+    };
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
@@ -28,12 +55,12 @@ export default function Footer() {
                     <div className={styles.form}>
                         <h1>Свяжитесь с нами</h1>
                         <div className={styles.flex}>
-                            <input placeholder="Имя" />
-                            <input placeholder="Номер телефона" />
+                            <input placeholder="Имя" value={name} onChange={(e) => setName(e.target.value)} />
+                            <input placeholder="Номер телефона" value={phone} onChange={(e) => setPhone(e.target.value)} />
                             <h2>Нажимая кнопку «Отправить»,<br />
                                 Вы даете свое согласие на обработку персональных данных.</h2>
                         </div>
-                        <Image className={styles.send} src={send} alt="" />
+                        <Image onClick={handleSubmit} className={styles.send} src={send} alt="" />
                     </div>
 
                     <div className={styles.onCart}>
